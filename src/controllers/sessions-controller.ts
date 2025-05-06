b7b4178a807e00d73106974fe48ddbd5644500c2
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-05 22:55:55 -0300

    feat: implement user authentication in session creation with JWT

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,42 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod"
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
-  create(req: Request, res: Response) {
-    return res.json({ message: "ok" })
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6)
+    })
+
+    const { email, password } = bodySchema.parse(req.body)
+
+    const user = await prisma.user.findFirst({ where: { email } })
+
+    if (!user) {
+      throw new AppError("Invalid or password!", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid or password!", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({ role: user.role ?? "member" }, secret, {
+      subject: user.id,
+      expiresIn
+    })
+
+    const { password: hashedPassword, ...userWithoutPassword } = user
+
+    return res.json({ token, user: userWithoutPassword })
   }
 }
 
