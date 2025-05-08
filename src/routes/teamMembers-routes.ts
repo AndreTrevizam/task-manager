3a57ceb6740368c5b36e1ba1f4c1dc6632839949
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-07 22:36:36 -0300

    feat: add team members routes with authentication and authorization

---
+++ src/routes/teamMembers-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { TeamMembersController } from "@/controllers/teamMembers-controller";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+
+const teamMembersRoutes = Router()
+const teamMembersController = new TeamMembersController()
+
+teamMembersRoutes.use(ensureAuthenticated, verifyUserAuthorization(['admin']))
+teamMembersRoutes.post("/", teamMembersController.create)
+teamMembersRoutes.delete("/:user_id/:team_id", teamMembersController.remove)
+teamMembersRoutes.get("/", teamMembersController.index)
+
+export { teamMembersRoutes }
