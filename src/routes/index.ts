1f0da2e8181cf1fa0bc7bcbbbdde8f56000ff271
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-07 23:45:12 -0300

    fix: correct import path for teamMembersRoutes and add tasksRoutes to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { teamsRoutes } from "./teams-routes";
-import { teamMembersRoutes } from "./teamMembers-routes";
+import { teamMembersRoutes } from "./team-members-routes";
+import { tasksRoutes } from "./tasks-routes";
 
 const routes = Router()
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 routes.use("/teams", teamsRoutes)
 routes.use("/team-members", teamMembersRoutes)
+routes.use("/tasks", tasksRoutes)
 
 export { routes }
