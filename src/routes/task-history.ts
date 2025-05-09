c211e13eaaf508ef30a0d48456fa2647ad8ca941
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-09 14:31:38 -0300

    feat: add taskHistoryRoutes with authentication and CRUD operations for task history management

---
+++ src/routes/task-history.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { TaskHistoryController } from "@/controllers/task-history-controller";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+
+const taskHistoryRoutes = Router()
+const taskHistoryController = new TaskHistoryController()
+
+taskHistoryRoutes.use(ensureAuthenticated, verifyUserAuthorization(['admin']))
+
+taskHistoryRoutes.post("/", taskHistoryController.create)
+taskHistoryRoutes.get("/:task_id/show", taskHistoryController.show)
+
+export { taskHistoryRoutes}
